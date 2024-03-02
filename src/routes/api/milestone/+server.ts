9fc1b8c88fa9a2e74271d2ecb2db4bbf39e8f88e
Chirayu31 <chirayupancholi17@gmail.com>
2024-03-02 20:54:35 +0530

    developed api to post milestone

---
+++ src/routes/api/milestone/+server.ts
@@ -0,0 +1,34 @@
+import { db } from '$lib/db.server.js';
+import { json, redirect } from '@sveltejs/kit';
+
+export async function POST(event) {
+  const data = await event.request.formData();
+  const projectId: string = data.get('projectId') as string;
+  const title: string = data.get('title') as string;
+  const description: string = data.get('description') as string;
+  const status: string = data.get('status') as string;
+
+  if (!projectId) {
+    throw redirect(303, '/');
+  }
+
+  try {
+    const milestone = await db.milestone.create({
+      data: {
+        title,
+        description,
+        status,
+        project: {
+        connect: {
+                id: "clt9rwjqx0001jvscvxihjzmt" 
+            }
+        }
+      },
+    });
+
+    return json(milestone);
+  } catch (error) {
+    console.error('Error creating milestone:', error);
+    return json({ error: 'Failed to create milestone' });
+  }
+}
